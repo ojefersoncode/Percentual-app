db28bc8b2b7799d38929231e6a0bac0006d676d1
ojefersoncode <jefersonvidal999@gmail.com>
2025-03-08 15:01:07 -0300

    Correção de erros de dependecias

--- components/misc/AuthForm.tsx
+++ components/misc/AuthForm.tsx
@@ -12,7 +12,6 @@ import {
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { useEffect, useState } from 'react';
-import { SiGithub, SiGoogle } from '@icons-pack/react-simple-icons';
 import { createApiClient } from '@/utils/supabase/api';
 import { createClient } from '@/utils/supabase/client';
 import { useToast } from '../ui/use-toast';
@@ -30,8 +29,8 @@ export function AuthForm({ state }: { state: AuthState }) {
   const [password, setPassword] = useState('');
   const stateInfo: Record<AuthState, StateInfo> = {
     signup: {
-      title: 'Sign Up',
-      submitText: 'Sign Up',
+      title: 'Cadastre-se',
+      submitText: 'Cadastrar',
       hasEmailField: true,
       hasPasswordField: true,
       hasOAuth: false,
@@ -54,8 +53,8 @@ export function AuthForm({ state }: { state: AuthState }) {
       }
     },
     signin: {
-      title: 'Sign In',
-      submitText: 'Sign In',
+      title: 'Entrar',
+      submitText: 'Entrar',
       hasEmailField: true,
       hasPasswordField: true,
       hasOAuth: true,
@@ -67,7 +66,7 @@ export function AuthForm({ state }: { state: AuthState }) {
         } catch (e) {
           if (e instanceof Error) {
             toast({
-              title: 'Auth Error',
+              title: 'Ocorreu um erro, verifique seus dados',
               description: e.message,
               variant: 'destructive'
             });
@@ -77,8 +76,8 @@ export function AuthForm({ state }: { state: AuthState }) {
       }
     },
     forgot_password: {
-      title: 'Reset Password',
-      submitText: 'Send Email',
+      title: 'Resetar Senha',
+      submitText: 'Enviar Email',
       hasEmailField: true,
       hasPasswordField: false,
       hasOAuth: false,
@@ -153,7 +152,7 @@ export function AuthForm({ state }: { state: AuthState }) {
 
   const currState = stateInfo[authState];
   return (
-    <Card className="mx-auto w-96 mx-4">
+    <Card className="mx-auto w-96 px-4">
       <CardHeader>
         <CardTitle className="text-2xl">{currState.title}</CardTitle>
         {currState.description && (
@@ -168,7 +167,7 @@ export function AuthForm({ state }: { state: AuthState }) {
               <Input
                 id="email"
                 type="email"
-                placeholder="m@example.com"
+                placeholder="email@example.com"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 disabled={loading}
@@ -179,19 +178,20 @@ export function AuthForm({ state }: { state: AuthState }) {
           {currState.hasPasswordField && (
             <div className="grid gap-2">
               <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
+                <Label htmlFor="password">Senha</Label>
                 {authState === 'signin' && (
                   <Link
                     href="#"
                     onClick={() => setAuthState(AuthState.ForgotPassword)}
                     className="ml-auto inline-block text-sm underline"
                   >
-                    Forgot your password?
+                    Esqueçeu a senha?
                   </Link>
                 )}
               </div>
               <Input
                 id="password"
+                placeholder='Digite sua senha'
                 type="password"
                 disabled={loading}
                 value={password}
@@ -210,13 +210,13 @@ export function AuthForm({ state }: { state: AuthState }) {
           </Button>
           {authState === 'signin' && (
             <div className="text-center text-sm">
-              Don&apos;t have an account?{' '}
+              Ainda não tem uma conta?{' '}
               <Link
                 href="#"
                 className="underline"
                 onClick={() => setAuthState(AuthState.Signup)}
               >
-                Sign up
+               cadastre-se
               </Link>
             </div>
           )}
@@ -252,23 +252,76 @@ export function AuthForm({ state }: { state: AuthState }) {
                 </div>
                 <div className="relative flex justify-center text-xs uppercase">
                   <span className="bg-background px-2 text-muted-foreground">
-                    Or continue with
+                    Ou continue com
                   </span>
                 </div>
               </div>
               <Button
                 variant="outline"
-                className="w-full"
+                className="w-full gap-2"
                 onClick={() => api.oauthSignin('google')}
               >
-                <SiGoogle className="h-4 w-4 mr-2" /> Google
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 48 48"
+                >
+                  <path
+                    fill="#FFC107"
+                    d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                  ></path>
+                  <path
+                    fill="#FF3D00"
+                    d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                  ></path>
+                  <path
+                    fill="#4CAF50"
+                    d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                  ></path>
+                  <path
+                    fill="#1976D2"
+                    d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                  ></path>
+                </svg>{' '}
+                Google
               </Button>
               <Button
                 variant="outline"
-                className="w-full"
+                className="w-full gap-2"
                 onClick={() => api.oauthSignin('github')}
               >
-                <SiGithub className="h-4 w-4 mr-2" /> Github
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="20"
+                  height="20"
+                  viewBox="0,0,256,256"
+                >
+                  <g
+                    fill="#ffffff"
+                    fill-rule="nonzero"
+                    stroke="none"
+                    stroke-width="1"
+                    stroke-linecap="butt"
+                    stroke-linejoin="miter"
+                    stroke-miterlimit="10"
+                    stroke-dasharray=""
+                    stroke-dashoffset="0"
+                    font-family="none"
+                    font-weight="none"
+                    font-size="none"
+                    text-anchor="none"
+                  >
+                    <g transform="scale(5.12,5.12)">
+                      <path d="M17.791,46.836c0.711,-0.306 1.209,-1.013 1.209,-1.836v-5.4c0,-0.197 0.016,-0.402 0.041,-0.61c-0.014,0.004 -0.027,0.007 -0.041,0.01c0,0 -3,0 -3.6,0c-1.5,0 -2.8,-0.6 -3.4,-1.8c-0.7,-1.3 -1,-3.5 -2.8,-4.7c-0.3,-0.2 -0.1,-0.5 0.5,-0.5c0.6,0.1 1.9,0.9 2.7,2c0.9,1.1 1.8,2 3.4,2c2.487,0 3.82,-0.125 4.622,-0.555c0.934,-1.389 2.227,-2.445 3.578,-2.445v-0.025c-5.668,-0.182 -9.289,-2.066 -10.975,-4.975c-3.665,0.042 -6.856,0.405 -8.677,0.707c-0.058,-0.327 -0.108,-0.656 -0.151,-0.987c1.797,-0.296 4.843,-0.647 8.345,-0.714c-0.112,-0.276 -0.209,-0.559 -0.291,-0.849c-3.511,-0.178 -6.541,-0.039 -8.187,0.097c-0.02,-0.332 -0.047,-0.663 -0.051,-0.999c1.649,-0.135 4.597,-0.27 8.018,-0.111c-0.079,-0.5 -0.13,-1.011 -0.13,-1.543c0,-1.7 0.6,-3.5 1.7,-5c-0.5,-1.7 -1.2,-5.3 0.2,-6.6c2.7,0 4.6,1.3 5.5,2.1c1.699,-0.701 3.599,-1.101 5.699,-1.101c2.1,0 4,0.4 5.6,1.1c0.9,-0.8 2.8,-2.1 5.5,-2.1c1.5,1.4 0.7,5 0.2,6.6c1.1,1.5 1.7,3.2 1.6,5c0,0.484 -0.045,0.951 -0.11,1.409c3.499,-0.172 6.527,-0.034 8.204,0.102c-0.002,0.337 -0.033,0.666 -0.051,0.999c-1.671,-0.138 -4.775,-0.28 -8.359,-0.089c-0.089,0.336 -0.197,0.663 -0.325,0.98c3.546,0.046 6.665,0.389 8.548,0.689c-0.043,0.332 -0.093,0.661 -0.151,0.987c-1.912,-0.306 -5.171,-0.664 -8.879,-0.682c-1.665,2.878 -5.22,4.755 -10.777,4.974v0.031c2.6,0 5,3.9 5,6.6v5.4c0,0.823 0.498,1.53 1.209,1.836c9.161,-3.032 15.791,-11.672 15.791,-21.836c0,-12.682 -10.317,-23 -23,-23c-12.683,0 -23,10.318 -23,23c0,10.164 6.63,18.804 15.791,21.836z"></path>
+                    </g>
+                  </g>
+                </svg>{' '}
+                Github
               </Button>
             </>
           )}
