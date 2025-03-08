f9bd555349836a714825a36ccfab791f91667834
ojefersoncode <jefersonvidal999@gmail.com>
2025-03-08 16:15:46 -0300

    Erros compilation

--- next.config.js
+++ next.config.js
@@ -8,3 +8,17 @@ module.exports = {
     ];
   }
 };
+
+
+// next.config.js
+module.exports = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+      '@components': path.resolve(__dirname, 'components'),
+      '@utils': path.resolve(__dirname, 'utils'),
+    };
+    return config;
+  },
+};
