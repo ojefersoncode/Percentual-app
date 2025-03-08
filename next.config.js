06c1c7e96c9e72cb1e941638d29926e1333652b9
ojefersoncode <jefersonvidal999@gmail.com>
2025-03-08 16:54:50 -0300

    Erros att build and imports

--- next.config.js
+++ next.config.js
@@ -1,30 +1,3 @@
-const path = require('path');
-
 module.exports = {
-  rewrites: async () => {
-    return [
-      {
-        source: '/auth',
-        destination: '/auth/signin'
-      }
-    ];
-  },
-  experimental: {
-    turboMode: true, // Habilita o Turbopack
-  },
-  webpack(config) {
-    // Se o Turbopack estiver ativo, não precisamos configurar o Webpack manualmente
-    if (process.env.NEXT_PUBLIC_TURBOPACK === 'true') {
-      // Remover ou ajustar configurações de Webpack conflitantes
-      return config;
-    }
-
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname),
-      '@components': path.resolve(__dirname, 'components'),
-      '@utils': path.resolve(__dirname, 'utils'),
-    };
-    return config;
-  },
-};
+  reactStrictMode: true,
+}
