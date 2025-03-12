88cfca9453be15e89f2b6a842a5c48890be17d9f
Jeferson  Code <134572153+ojefersoncode@users.noreply.github.com>
2025-03-11 22:45:09 -0300

    Update HowItWorks.tsx

--- components/landing/HowItWorks.tsx
+++ components/landing/HowItWorks.tsx
@@ -45,9 +45,9 @@ export const HowItWorks = () => {
   return (
     <section id="howItWorks" className="container text-center py-14">
       <h2 className="w-full px-4 text-4xl font-bold">
-        Por que escolher{' '}
+        Por que escolher a{' '}
         <span className="bg-gradient-to-b from-red-500 to-red-700 text-white px-2 rounded-lg">
-          Codersaas?
+          CoderSaaS?
         </span>
       </h2>
       <p className="md:w-3/4 mx-auto mt-4 mb-8 text-lg text-muted-foreground">
