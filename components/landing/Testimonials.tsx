e4024323d53e73b6941e3a8cb273d9da70e1c4cf
Ojefersoncode <jefersonvidal999@gmail.com>
2025-03-13 19:00:33 -0300

    Resolvido conflito em Testimonials.tsx

--- components/landing/Testimonials.tsx
+++ components/landing/Testimonials.tsx
@@ -20,22 +20,14 @@ const testimonials: TestimonialProps[] = [
     image: 'https://github.com/shadcn.png',
     name: 'John Doe React',
     userName: '@john_Doe',
-<<<<<<< HEAD
-    comment: 'This landing page is awesome!'
-=======
     comment: 'This landing page is awesome! labore et dolore magna aliqua.'
->>>>>>> 1b18d35 (update)
   },
   {
     image: 'https://github.com/shadcn.png',
     name: 'John Doe React',
     userName: '@john_Doe1',
     comment:
-<<<<<<< HEAD
-      'Lorem ipsum dolor sit amet,empor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.'
-=======
       'Lorem ipsum dolor sit amet,empor incididunt ut labore et dolore magna aliqua.'
->>>>>>> 1b18d35 (update)
   },
 
   {
@@ -43,64 +35,34 @@ const testimonials: TestimonialProps[] = [
     name: 'John Doe React',
     userName: '@john_Doe2',
     comment:
-<<<<<<< HEAD
-      'Lorem ipsum dolor sit amet,exercitation. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.'
-=======
       'Lorem ipsum dolor sit amet,exercitation. Duis aute irure dolor in reprehenderit. '
->>>>>>> 1b18d35 (update)
   },
   {
     image: 'https://github.com/shadcn.png',
     name: 'John Doe React',
     userName: '@john_Doe3',
     comment:
-<<<<<<< HEAD
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
-=======
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.'
->>>>>>> 1b18d35 (update)
   },
   {
     image: 'https://github.com/shadcn.png',
     name: 'John Doe React',
     userName: '@john_Doe4',
     comment:
-<<<<<<< HEAD
       'Lorem ipsum dolor sit amet, tempor incididunt  aliqua. Ut enim ad minim veniam, quis nostrud.'
-=======
-      'Lorem ipsum dolor sit amet, tempor incididunt  aliqua. Ut enim ad minim veniam.'
->>>>>>> 1b18d35 (update)
   },
   {
     image: 'https://github.com/shadcn.png',
     name: 'John Doe React',
     userName: '@john_Doe5',
     comment:
-<<<<<<< HEAD
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-=======
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt '
->>>>>>> 1b18d35 (update)
   }
 ];
 
 export const Testimonials = () => {
   return (
-<<<<<<< HEAD
-    <section id="testimonials" className="container py-14 sm:py-20">
-      <h2 className="text-3xl md:text-4xl font-bold">
-        Discover Why
-        <span className="text-red-600 bg-clip-text">
-          {' '}
-          People Love{' '}
-        </span>
-        This Landing Page
-      </h2>
 
-      <p className="text-xl text-muted-foreground pt-4 pb-8">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non unde error
-        facere hic reiciendis illo
-=======
     <section id="testimonials" className="container py-14">
       <h2 className="text-3xl md:text-4xl font-bold">
         Veja alguns de nossos
@@ -110,7 +72,6 @@ export const Testimonials = () => {
 
       <p className="text-xl text-muted-foreground pt-4 pb-8">
         Confira agora algumas avaliações de nossos usuarios, estão achando da nossa plataforma.
->>>>>>> 1b18d35 (update)
       </p>
 
       <div className="grid md:grid-cols-2 lg:grid-cols-4 sm:block columns-2  lg:columns-3 lg:gap-6 mx-auto space-y-4 lg:space-y-6">
