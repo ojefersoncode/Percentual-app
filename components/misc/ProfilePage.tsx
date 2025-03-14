9be77d7808e8ac570af6b0491f3f558033a81d35
Ojefersoncode <jefersonvidal999@gmail.com>
2025-03-14 16:56:05 -0300

    Add new style profile page button

--- components/misc/ProfilePage.tsx
+++ components/misc/ProfilePage.tsx
@@ -10,9 +10,10 @@ import { Button } from '../../components/ui/button';
 import { Label } from '../../components/ui/label';
 import { Navbar } from '../HomeComponents/NavBar';
 import { User } from '@supabase/supabase-js';
+import { Pencil } from 'lucide-react';
+import { Footer } from '../landing/Footer';
 
 export default function ProfilePage({ user }: { user: User }) {
-  
   const [name, setName] = useState('Jeferson code');
   const [phone, setPhone] = useState('+55 31 91234-5678');
   const [email, setEmail] = useState('ojefersoncode@email.com');
@@ -28,7 +29,7 @@ export default function ProfilePage({ user }: { user: User }) {
     <div className="flex flex-col w-full mx-auto bg-gray-100 dark:bg-black dark:bg-muted/40 min-h-screen">
       {/* Navbar */}
       <nav className="flex w-full justify-between py-4 px-4 md:px-8 bg-black">
-        <div className="flex gap-2 text-xl font-bold items-center">
+        <div className="flex gap-2 text-xl font-bold items-center select-none">
           <img className="h-8 w-8" src="/Leptrum.png" alt="logo" />
           <div className="flex">
             <h1 className="text-slate-50">Codersaas</h1>
@@ -37,11 +38,9 @@ export default function ProfilePage({ user }: { user: User }) {
         <Navbar />
       </nav>
 
-      <h1 className="flex mx-10 text-black dark:text-white text-2xl font-bold text-center md:text-left mt-8">Perfil</h1>
-
       {/* Card Container */}
-      <div className="flex justify-center my-6">
-        <Card className="w-full max-w-6xl p-6 md:p-8 bg-white dark:bg-black shadow-lg rounded-lg">
+      <div className="flex justify-center my-10">
+        <Card className="w-full max-w-4xl p-6 md:p-8 bg-white dark:bg-black shadow-lg rounded-lg">
           <CardHeader>
             <h2 className="text-xl font-medium">Meu perfil</h2>
           </CardHeader>
@@ -49,55 +48,74 @@ export default function ProfilePage({ user }: { user: User }) {
           <CardContent>
             {/* Name */}
             <div className="flex justify-between items-center space-x-4 mb-4">
-              <Label htmlFor="name" className="text-lg">Nome:</Label>
-              <div className="flex items-center space-x-2">
+              <Label htmlFor="name" className="text-lg">
+                Nome:
+              </Label>
+              <div className="flex items-center space-x-3">
                 <span className="text-gray-600 dark:text-gray-300">{name}</span>
                 <Button
+                variant={'outline'}
                   onClick={() =>
                     handleEdit('name', prompt('Enter new name:', name) || name)
                   }
                   className="text-sm"
                 >
-                  Editar
+                  <Pencil className="size-4" />
                 </Button>
               </div>
             </div>
 
             {/* Phone */}
             <div className="flex justify-between items-center space-x-4 mb-4">
-              <Label htmlFor="phone" className="text-lg">Telefone:</Label>
-              <div className="flex items-center space-x-2">
-                <span className="text-gray-600 dark:text-gray-300">{phone}</span>
+              <Label htmlFor="phone" className="text-lg">
+                Telefone:
+              </Label>
+              <div className="flex items-center space-x-3">
+                <span className="text-gray-600 dark:text-gray-300">
+                  {phone}
+                </span>
                 <Button
+                variant={'outline'}
                   onClick={() =>
-                    handleEdit('phone', prompt('Enter new phone number:', phone) || phone)
+                    handleEdit(
+                      'phone',
+                      prompt('Enter new phone number:', phone) || phone
+                    )
                   }
                   className="text-sm"
                 >
-                  Editar
+                  <Pencil className="size-4" />
                 </Button>
               </div>
             </div>
 
             {/* Email */}
             <div className="flex justify-between items-center space-x-4 mb-4">
-              <Label htmlFor="email" className="text-lg">Email:</Label>
+              <Label htmlFor="email" className="text-lg">
+                Email:
+              </Label>
               <span className="text-gray-600 dark:text-gray-300">{email}</span>
             </div>
 
             {/* ID */}
             <div className="flex justify-between items-center space-x-4 mb-4">
-              <Label htmlFor="id" className="text-lg">ID:</Label>
+              <Label htmlFor="id" className="text-lg">
+                ID:
+              </Label>
               <span className="text-gray-600 dark:text-gray-300">{id}</span>
             </div>
           </CardContent>
 
           <CardFooter>
             <Button variant="outline" className="w-full md:w-auto mt-4">
-              Savar mudanças
+              Savar Alterações
             </Button>
           </CardFooter>
         </Card>
+      </div>
+
+      <div className="bg-black">
+        <Footer />
       </div>
     </div>
   );
