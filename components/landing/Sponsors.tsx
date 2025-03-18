40a3f47f48758f4bff9417b1d7bcc969b17e9c90
Ojefersoncode <jefersonvidal999@gmail.com>
2025-03-18 15:38:56 -0300

    Add scroll bar personalizada

--- components/landing/Sponsors.tsx
+++ components/landing/Sponsors.tsx
@@ -9,22 +9,22 @@ interface SponsorProps {
 
 const sponsors: SponsorProps[] = [
   {
-    icon: <img src="/nextjs.svg" alt="Next js logo" className="w-32 h-44" />,
+    icon: <img src="/nextjs.svg" alt="Next js logo" className="w-32 h-20" />,
     description:
       'Next.js é um framework de React para a criação de aplicações web de alta performance, com renderização do lado do servidor e otimizações automáticas.',
   },
   {
-    icon: <img src="/supabase.svg" alt="Supabase logo" className="w-48 h-44" />,
+    icon: <img src="/supabase.svg" alt="Supabase logo" className="w-48 h-20" />,
     description:
       'Supabase é uma plataforma de código aberto que fornece backend como serviço, permitindo criar aplicativos com banco de dados e autenticação prontos para usar.',
   },
   {
-    icon: <img src="/vercel.svg" alt="Vercel logo" className="w-32 h-44" />,
+    icon: <img src="/vercel.svg" alt="Vercel logo" className="w-32 h-20" />,
     description:
       'Vercel é uma plataforma para deployment de front-end, otimizada para aplicações React e Next.js, oferecendo uma entrega global de alta performance.',
   },
   {
-    icon: <img src="/stripe.svg" alt="Stripe logo" className="w-32 h-44 " />,
+    icon: <img src="/stripe.svg" alt="Stripe logo" className="w-32 h-20" />,
     description:
       'Stripe é uma plataforma de pagamento online que facilita a integração de pagamentos de forma simples e segura em qualquer tipo de aplicação web.',
   },
@@ -34,19 +34,18 @@ export const Sponsors = () => {
   return (
     <section id="sponsors" className="container text-center pb-12 max-md:pt-12">
       <hr className='w-full' />
-      <h2 className="text-2xl md:text-6xl font-bold text-red-50 mt-8 mb-8 md:mb-12">
-        Stack que usamos nos projetos
+      <h2 className="text-3xl md:text-5xl font-bold text-red-50 pt-4 mt-8 mb-8 md:mb-12">
+        Aqui usamos as melhores tecnologias do mercado!
       </h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 justify-center">
-        {/* Mapeando e criando os cards */}
         {sponsors.map(({ icon, description }, index) => (
           <div
             key={index}
-            className="flex flex-col items-center justify-between bg-red-600 rounded-lg shadow-lg p-6 hover:shadow-xl transition-all duration-300 min-h-[300px]" // Definindo uma altura mínima
+            className="flex pt-10 flex-col items-center justify-between bg-red-600 rounded-lg shadow-lg p-6 hover:shadow-xl transition-all duration-300 min-h-[300px]"
           >
-            <div className="flex w-full  justify-center items-center">{icon}</div>
-            <p className="text-sm text-center text-gray-200 pb-10">{description}</p>
+            <div className="flex w-full justify-center items-center">{icon}</div>
+            <p className="text-sm text-center text-gray-200 max-md:px-4 pb-10">{description}</p>
           </div>
         ))}
       </div>
