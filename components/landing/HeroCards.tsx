dfe7f4fda7ea86340eaab4269067643dc992e2df
Jeferson  Code <134572153+ojefersoncode@users.noreply.github.com>
2025-03-21 01:51:06 -0300

    Update HeroCards.tsx

--- components/landing/HeroCards.tsx
+++ components/landing/HeroCards.tsx
@@ -5,7 +5,7 @@ export const HeroCards = () => {
     <div className="lg:flex flex-row flex-wrap gap-8 w-full h-auto max-md:my-16">
       <div className="flex justify-center items-center md:pb-36">
         <img
-          src="/IMG_20250320_014555_278.webp"
+          src="/COODERFY.png"
           alt="Cooderfy"
           className="absolute w-1/2 pr-20 max-md:w-full max-md:px-6 rounded-lg drop-shadow-lg object-cover"
         />
