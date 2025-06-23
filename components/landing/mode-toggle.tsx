a806fd6dcaba3450899b5db198c234c502d811b4
Jeferson  Code <134572153+ojefersoncode@users.noreply.github.com>
2025-06-23 19:24:50 -0300

    Update mode-toggle.tsx

--- components/landing/mode-toggle.tsx
+++ components/landing/mode-toggle.tsx
@@ -20,8 +20,8 @@ export function ModeToggle() {
           size="icon"
           className="ghost bg-white text-black dark:bg-black dark:hover:bg-black dark:hover:text-white dark:text-white"
         >
-          <Sun className="h-[1.1rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.1rem] w-[1.2rem] rotate-90 scale-0 :rotate-0 dark:scale-100 transition-all" />
+          <Sun className="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute size-4 rotate-90 scale-0 :rotate-0 dark:scale-100 transition-all" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
