32eb3529044ad2d38e5384f69ca9c7561a1c4316
Jeferson  Code <134572153+ojefersoncode@users.noreply.github.com>
2025-06-23 19:49:44 -0300

    Update mode-toggle.tsx

--- components/landing/mode-toggle.tsx
+++ components/landing/mode-toggle.tsx
@@ -17,8 +17,7 @@ export function ModeToggle() {
       <DropdownMenuTrigger asChild>
         <Button
           variant="ghost"
-          size="icon"
-          className="ghost border border-border p-1 bg-white text-black dark:bg-black dark:hover:bg-black dark:hover:text-white dark:text-white"
+          className="ghost rounded-full border border-gray-200 dark:border-gray-700 p-1 bg-white text-black dark:bg-black dark:hover:bg-black dark:hover:text-white dark:text-white"
         >
           <Sun className="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute size-4 rotate-90 scale-0 :rotate-0 dark:scale-100 transition-all" />
