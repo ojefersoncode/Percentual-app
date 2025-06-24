95e7a336b10300422f6a2bfca897f4b95f4d4a52
Jeferson  Code <134572153+ojefersoncode@users.noreply.github.com>
2025-06-23 23:47:38 -0300

    Update Componentes.tsx

--- components/pages/Componentes.tsx
+++ components/pages/Componentes.tsx
@@ -51,16 +51,14 @@ export default function Componentes({ user }: { user: User }) {
           <div className="flex w-full items-center justify-between py-2">
           <h1 className="font-bold text-2xl">Botões</h1>
           <div className="flex gap-2">
-          <Button
-          variant={'default'}
-          className="px-2 py-1 bg-white dark:bg-background text-black dark:text-white  hover:bg-transparent border border-gray-500/40">
+          <button
+          className="px-3 py-2 bg-white dark:bg-background text-black dark:text-white  hover:bg-transparent border border-gray-500/40">
           <MoveLeft className="size-3" />
-          </Button>
-          <Button 
-          variant={'default'}
-          className="px-2 py-1 bg-white dark:bg-background text-black dark:text-white  hover:bg-transparent border border-gray-500/40">
+          </button>
+          <button
+          className="px-3 py-2 bg-white dark:bg-background text-black dark:text-white  hover:bg-transparent border border-gray-500/40">
           <MoveRight className="size-3" />
-          </Button>
+          </button>
           </div>
           </div>
           
