7830d3508b8fc3af9e8b3234f550a4d52a43e6e6
Jeferson  Code <134572153+ojefersoncode@users.noreply.github.com>
2025-06-23 22:24:28 -0300

    Update Componentes.tsx

--- components/pages/Componentes.tsx
+++ components/pages/Componentes.tsx
@@ -90,11 +90,11 @@ export default function Componentes({ user }: { user: User }) {
         <div className="border p-4 bg-subbackground rounded-lg w-full py-4">
           <h1 className="font-bold text-lg pb-4">Botão outiline</h1>
 
-          <div className="flex w-full items-center gap-4 p-2">
-            <h2>Preview outline:</h2>
+          <div className="flex w-full items-center bg-background gap-4 p-2">
+            <h2>Preview</h2>
             <Button
               variant={'outline'}
-              className="bg-gray-500 text-white p-2 rounded"
+              className="bg-white text-black p-4 rounded"
             >
               Entrar
             </Button>
@@ -121,11 +121,11 @@ export default function Componentes({ user }: { user: User }) {
         <div className="border p-4 bg-subbackground rounded-lg w-full my-4">
           <h1 className="font-bold text-lg pb-4">Botão solido</h1>
 
-          <div className="flex w-full items-center gap-4 p-2">
-            <h2>Preview ghost:</h2>
+          <div className="flex w-full items-center gap-4 bg-background p-2">
+            <h2>Preview</h2>
             <Button
               variant={'ghost'}
-              className="bg-gray-950 text-white p-2 rounded"
+              className="bg-white text-black p-4 rounded"
             >
               Sair
             </Button>
