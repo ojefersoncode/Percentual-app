9c8ea07a150dd9299af1db2de0f4644d111022ff
Jeferson  Code <134572153+ojefersoncode@users.noreply.github.com>
2025-06-24 02:45:17 -0300

    Update Download.tsx

--- components/HomeComponents/Download.tsx
+++ components/HomeComponents/Download.tsx
@@ -5,7 +5,7 @@ import { HeroCards } from '../landing/HeroCards';
 
 export const Download = () => {
   return (
-    <section className="container grid lg:grid-cols-2 place-items-center px-2 py-14 md:pt-28 gap-8">
+    <section className="container bg-background grid lg:grid-cols-2 place-items-center px-2 py-14 md:pt-28 gap-8">
       <div className="text-center lg:text-start mx-7 max-md:mx-4 space-y-4">
         <main className="text-4xl md:text-6xl font-bold">
           <h1 className="inline">
