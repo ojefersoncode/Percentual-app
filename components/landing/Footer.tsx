b822036b311139f24360d7cc6b8938cfea52bcec
Jeferson  Code <134572153+ojefersoncode@users.noreply.github.com>
2025-06-24 05:54:09 -0300

    Update Footer.tsx

--- components/landing/Footer.tsx
+++ components/landing/Footer.tsx
@@ -14,7 +14,7 @@ export const Footer = () => {
   return (
     <footer id="footer" className="bg-white dark:bg-background text-black dark:text-gray-100">
       {/* Seção Principal */}
-      <section className="container py-10 grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
+      <section className="container py-4 grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-4">
         
         {/* Logo */}
         <a
