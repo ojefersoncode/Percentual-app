361d68e6c0323ce862e063d066bd9ff75dc8e61b
Jeferson  Code <134572153+ojefersoncode@users.noreply.github.com>
2025-06-24 19:22:41 -0300

    Update Componentes.tsx

--- components/pages/Componentes.tsx
+++ components/pages/Componentes.tsx
@@ -31,9 +31,9 @@ export default function Componentes({ user }: { user: User }) {
       {/* Navbar */}
       <nav className="flex w-full justify-between py-2 max-md:px-3 md:px-8 bg-white dark:bg-background border-b border-gray-600/40">
         <div className="flex gap-1 text-xl font-bold items-center">
-          <img className="size-8" src="/logo.webp" alt="logo" />
+          <img className="size-8" src="/LogoC.png" alt="logo" />
           <div className="flex">
-            <h1 className="text-gray-900 dark:text-gray-100 text-base">
+            <h1 className="text-gray-900 dark:text-gray-100 font-sans text-base">
               Cooderfy
             </h1>
           </div>
