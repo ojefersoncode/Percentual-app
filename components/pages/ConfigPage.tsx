db58093bbdabe530c92c517a26ebe6e51d7b25f5
Jeferson  Code <134572153+ojefersoncode@users.noreply.github.com>
2025-06-24 22:24:04 -0300

    Update ConfigPage.tsx

--- components/pages/ConfigPage.tsx
+++ components/pages/ConfigPage.tsx
@@ -131,7 +131,7 @@ export default function ConfigPage({
         </div>
       </div>
 
-      <div className="w-full mt-7">
+      <div className="w-full pt-4">
         <Footer />
       </div>
     </div>
