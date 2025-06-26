e076e5d7384b20d10a62754853343cbdaeff7225
Jeferson  Code <134572153+ojefersoncode@users.noreply.github.com>
2025-06-26 01:18:47 -0300

    Update HomePage.tsx

--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -8,7 +8,7 @@ import { Download } from '../HomeComponents/Download';
 
 export default function HomePage({ user }: { user: User }) {
   return (
-    <div className="relative flex min-h-screen w-full bg-gray-200 dark:bg-black dark:bg-muted/40">
+    <div className="relative flex min-h-screen w-full white dark:bg-background">
       <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col">
         <nav className="flex w-full justify-between py-2 max-md:px-3 md:px-8 bg-white dark:bg-background border-b border-gray-200 dark:border-border">
           <div className="flex gap-1 text-lg font-sans items-center">
@@ -26,7 +26,7 @@ export default function HomePage({ user }: { user: User }) {
           </div>
         </nav>
 
-        <div className="bg-background">
+        <div>
           <Download />
         </div>
 
