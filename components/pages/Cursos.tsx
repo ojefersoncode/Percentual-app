80bec8cfc074d0e77cddfbbbce16086d64d31e3b
Cooderfy <134572153+ojefersoncode@users.noreply.github.com>
2025-06-27 05:16:07 -0300

    Update Cursos.tsx

--- components/pages/Cursos.tsx
+++ components/pages/Cursos.tsx
@@ -17,7 +17,7 @@ export default function Cursos({ user }: { user: User }) {
   ];
 
   return (
-    <div className="relative flex min-h-screen w-full bg-white dark:bg-background">
+    <div className="relative flex min-h-screen w-full bg-white dark:bg-background touch-pan-x touch-pan-y">
       <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col">
         <nav className="flex w-full justify-between py-2 max-md:px-3 md:px-8 bg-white dark:bg-background border-b border-gray-500/40 dark:border-border">
           <div className="flex gap-1 text-lg font-sans items-center">
