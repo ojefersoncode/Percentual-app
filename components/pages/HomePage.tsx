e8e8ef4f11e02f6a1170f37959f4e298ff6d2316
Cooderfy <134572153+ojefersoncode@users.noreply.github.com>
2025-06-28 10:12:21 -0300

    Update HomePage.tsx

--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -11,11 +11,14 @@ export default function HomePage({ user }: { user: User }) {
     <div className="flex min-h-screen w-full white dark:bg-background touch-pan-x touch-pan-y">
       <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col">
         <nav className="flex w-full justify-between py-2 max-md:px-3 md:px-4 bg-white dark:bg-background border-b border-gray-200 dark:border-border">
-          <div className="flex gap-1 text-lg font-sans items-center">
-            <img className="size-7" src="/LogoC.png" alt="logo" />
+          <div className="flex gap-1 items-center">
+            <img 
+              className="size-5 rounded-full" 
+              src="/User.jpeg" 
+              alt="logo" />
             <div className="flex">
               <h1 className="text-gray-900 dark:text-gray-100 text-base">
-                Cooderfy
+                Jeferson
               </h1>
             </div>
           </div>
