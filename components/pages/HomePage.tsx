c51aa690f682f0b42e0f132d0a0853edb6a4fcfb
Cooderfy <134572153+ojefersoncode@users.noreply.github.com>
2025-06-28 06:45:52 -0300

    Update HomePage.tsx

--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -8,7 +8,7 @@ import { Download } from '../HomeComponents/Download';
 
 export default function HomePage({ user }: { user: User }) {
   return (
-    <div className="relative flex min-h-screen w-full white dark:bg-background touch-pan-x touch-pan-y">
+    <div className="relative flex h-screen w-full white dark:bg-background touch-pan-x touch-pan-y">
       <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col">
         <nav className="flex w-full justify-between py-2 max-md:px-3 md:px-8 bg-white dark:bg-background border-b border-gray-200 dark:border-border">
           <div className="flex gap-1 text-lg font-sans items-center">
