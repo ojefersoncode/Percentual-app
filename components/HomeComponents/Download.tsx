4308d88be6f8072c391e8b402fa7ee30fc8bf0c3
Cooderfy <134572153+ojefersoncode@users.noreply.github.com>
2025-06-28 06:24:42 -0300

    Update Download.tsx

--- components/HomeComponents/Download.tsx
+++ components/HomeComponents/Download.tsx
@@ -1,13 +1,30 @@
 'use client';
 import Link from 'next/link';
 import { Button } from '../../components/ui/button';
-import { Card, CardContent } from '../../components/ui/card'; // Se você tiver um componente Card, use ele.
-import { Clock, GitBranch } from 'lucide-react'; // Ícones parecidos com os da Vercel
+import { Input } from '../../components/ui/Input';
+import { Card, CardContent } from '../../components/ui/card';
+import { Clock, GitBranch, Search, Plus } from 'lucide-react';
 
 export const Download = () => {
   return (
     <section className="container bg-white dark:bg-background grid lg:grid-cols-2 place-items-center px-2 py-14 md:pt-28 gap-8">
+
+      <div className="flex w-full gap-4 items-center">
+         <Input
+           type="search"
+           placeholder="Pesquisar..."
+           className=""
+         />
+
+        <Button className="p-4 rounded-lg">
+          <Search />
+        </Button>
+
+        <Button className="p-4 rounded-lg">
+          <Plus />
+        </Button>
       
+      </div>
 
       {/* Seção dos Projetos estilo Vercel */}
       <div className="flex flex-col gap-4 w-full max-w-lg">
