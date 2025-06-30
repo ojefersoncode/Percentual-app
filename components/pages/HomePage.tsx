8611cf39c1935680448629d032dc0409ddc25d21
Cooderfy <134572153+ojefersoncode@users.noreply.github.com>
2025-06-30 07:27:45 -0300

    Update HomePage.tsx

--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -10,7 +10,7 @@ export default function HomePage({ user }: { user: User }) {
   return (
     <div className="flex min-h-screen w-full white dark:bg-background drop-shadow-md touch-pan-x touch-pan-y">
       <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col">
-        <nav className="flex w-full justify-between py-2 max-md:px-3 md:px-4 bg-white dark:bg-background border-b border-gray-200 dark:border-border">
+        <nav className="flex w-full justify-between py-2 max-md:px-3 md:px-4 bg-white dark:bg-background border-b border-zinc-300 dark:border-border">
           <div className="flex items-center">
             <img 
               className="size-8 rounded-full" 
