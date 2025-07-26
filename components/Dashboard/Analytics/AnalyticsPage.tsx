45fd61f71d977cb4172492a6d207e0adebdf7834
ojefersoncode <Jefersonvidal999@gmail.com>
2025-07-26 05:17:55 -0300

    Edit dashboard ANALYTICS AND ADD TABLE

--- components/Dashboard/Analytics/AnalyticsPage.tsx
+++ components/Dashboard/Analytics/AnalyticsPage.tsx
@@ -1,16 +1,20 @@
 'use client';
 
-import { Separator } from '@/components/ui/separator';
 import { TableUser } from '../Tables/TableUser';
 import { OnlineUser } from './OnlineUser';
 import { TotalUser } from './TotalUser';
 import { ProfileButton } from '@/components/HomeComponents/ProfileButton';
-import { ScrollBar } from '@/components/ui/scroll-area';
+
 
 export const AnalyticsPage = () => {
   return (
     <section className="flex flex-col bg-white dark:bg-background p-4">
-
+      <div className="flex w-full justify-between items-center pb-2">
+        <h1 className="text-xl font-bold text-black dark:text-text">
+          Dashboard
+        </h1>
+        <ProfileButton />
+      </div>
 
       <div className="grid md:grid-cols-2 items-center gap-4">
         <OnlineUser />
@@ -21,7 +25,7 @@ export const AnalyticsPage = () => {
         <h2 className="text-lg font-bold">Tabela de usuario</h2>
       </div>
 
-      <div className="p-1 w-full">
+      <div className="w-full flex-1">
         <TableUser />
       </div>
     </section>
--- components/Dashboard/Tables/TableUser.tsx
+++ components/Dashboard/Tables/TableUser.tsx
@@ -1,9 +1,8 @@
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow
@@ -49,34 +48,52 @@ const invoices = [
 
 export function TableUser() {
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Email</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead className="text-right">Request</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {invoices.map((invoice) => (
-          <TableRow key={invoice.email}>
-            <TableCell className="font-medium cursor-pointer">
-              {invoice.email}
-            </TableCell>
-            <TableCell>
-              <div className="flex items-center max-md:justify-center gap-2">
-                <span
-                  className={`h-2 w-2 rounded-full ${
-                    invoice.Status === 'Online' ? 'bg-green-600' : 'bg-red-600'
-                  }`}
-                ></span>
-                <span className='max-md:hidden'>{invoice.Status}</span>
-              </div>
-            </TableCell>
-            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+    <ScrollArea>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Email</TableHead>
+            <TableHead className="max-md:hidden">Status</TableHead>
+            <TableHead className="text-right">Request</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoice) => (
+            <TableRow key={invoice.email}>
+              <TableCell className="font-medium cursor-pointer">
+                <div className='flex items-center gap-2'>     
+                  <span
+                    className={`h-2 w-2 md:hidden rounded-full ${
+                      invoice.Status === 'Online'
+                        ? 'bg-green-600'
+                        : 'bg-red-600'
+                    }`}
+                  ></span>
+                  <span> {invoice.email} </span>
+             
+                </div>
+              </TableCell>
+
+              <TableCell className="max-md:hidden">
+                <div className="flex items-center max-md:justify-center gap-2">
+                  <span
+                    className={`h-2 w-2 rounded-full ${
+                      invoice.Status === 'Online'
+                        ? 'bg-green-600'
+                        : 'bg-red-600'
+                    }`}
+                  ></span>
+                  <span className="max-md:hidden">{invoice.Status}</span>
+                </div>
+              </TableCell>
+              <TableCell className="text-right">
+                {invoice.totalAmount}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
   );
 }
