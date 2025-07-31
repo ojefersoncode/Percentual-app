4e2e62d706985985b776a38fdd32adb272635de0
ojefersoncode <Jefersonvidal999@gmail.com>
2025-07-31 09:46:26 -0300

    Edit Landing page

--- components/landing/Actionleads.tsx
+++ components/landing/Actionleads.tsx
@@ -5,17 +5,15 @@ export default function Actionleads() {
   return (
     <div className="flex gap-4">
       <Button
-        variant="ghost"
-        className="rounded-lg p-3 max-md:p-1 w-44 max-md:w-32 max-md:text-xs text-background dark:text-text dark:bg-background 
-        border hover:bg-white/80 dark:hover:bg-white/5 transition-all"
+        className="rounded-lg p-3 max-md:p-1 w-44 max-md:w-32 max-md:text-xs text-background dark:text-text bg-slate-100 dark:bg-background 
+        border border-zinc-400 dark:border-zinc-700 hover:bg-white/80 dark:hover:bg-white/5 transition-all"
         type="submit"
       >
         Saiba mais
       </Button>
       <Button
-        variant="ghost"
         className="rounded-lg p-3 max-md:p-1 w-44 max-md:w-32 max-md:text-xs text-text dark:text-text
-         bg-btn dark:bg-btn/80 hover:bg-btn/90 dark:hover:bg-btn transition-all"
+         border-none bg-btn dark:bg-btn/80 hover:bg-btn/90 dark:hover:bg-btn transition-all"
         type="submit"
       >
         Cadastre-se
