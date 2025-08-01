eacc5796ca84e36feafa40a8b23bfca0655c4b85
ojefersoncode <Jefersonvidal999@gmail.com>
2025-08-01 13:54:30 -0300

    Edit Landing page and card image

--- components/landing/HeroCards.tsx
+++ components/landing/HeroCards.tsx
@@ -6,8 +6,8 @@ export const HeroCards = () => {
   const { theme } = useTheme(); // Pega o tema atual
 
   return (
-    <div className="w-full">
-      <div className="flex mx-2 rounded-t-lg border-t border-r border-l border-zinc-400 dark:border-zinc-700">
+    <div className="w-full pt-4 max-sm:pt-10">
+      <div className="flex rounded-t-lg border-t border-r border-l border-zinc-400 dark:border-zinc-700">
         <img
           src={theme === 'dark' ? '/Plataforma.png' : '/plataforma2.png'}
           alt="Cooderfy"
