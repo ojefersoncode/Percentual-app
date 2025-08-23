976210c3566db8091efe7c72a238475ecf6b56ae
ojefersoncode <Jefersonvidal999@gmail.com>
2025-08-23 16:28:32 -0300

    Add checkout

--- app/Home/layout.tsx
+++ app/Home/layout.tsx
@@ -2,20 +2,22 @@ import Navbar from '@/components/HomeComponents/Navbar';
 import { Navbottom } from '@/components/HomeComponents/Navbottom';
 import { ThemeProvider } from '@/components/landing/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
+import { User } from '@supabase/supabase-js';
 
-export default function HomeLayout({
-  children
-}: Readonly<{
+interface HomeLayoutProps {
   children: React.ReactNode;
-}>) {
+  user: User;
+}
+
+export default function HomeLayout({ children, user }: HomeLayoutProps) {
   return (
     <ThemeProvider>
       <main
         id="skip"
-        className="min-h-[calc(100dvh-4rem)] md:min-h[calc(100dvh-5rem)]"
+        className="min-h-[calc(100dvh-4rem)] md:min-h-[calc(100dvh-5rem)]"
       >
         <div>
-          <Navbar />
+          <Navbar user={user} />
         </div>
 
         <div className="md:hidden z-40">
