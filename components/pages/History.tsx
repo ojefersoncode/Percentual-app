448b2bdac810667b44c41574c560248db5ce2005
ojefersoncode <Jefersonvidal999@gmail.com>
2025-08-25 16:02:34 -0300

    Add history page

--- components/pages/History.tsx
+++ components/pages/History.tsx
@@ -1,19 +1,16 @@
 'use client';
 
 import { User } from '@supabase/supabase-js';
-import Cards from '../HomeComponents/Cards';
 import Footer from '../HomeComponents/Footer';
 import GiftBox from '../HistoryComponents/GiftsBox';
 import { BalanceHistory } from '../HistoryComponents/BalanceHistory';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/tabs';
-import DepositModal from '../WithdrawComponents/Depositmodal';
-import Withdraw from '../WithdrawComponents/Withdraw';
 
 export default function HistoryPage({ user }: { user: User }) {
   return (
     <div className="px-4 pt-4 max-md:pb-24">
       <div className="pb-4">
-        <Tabs defaultValue="account">
+        <Tabs defaultValue="Meuspresentes">
           <TabsList className="bg-subbackground dark:bg-subbackground">
             <TabsTrigger
               className="rounded-sm bg-background dark:bg-subbackground hover:bg-subbackground dark:hover:bg-subbackground text-text dark:text-text"
